8bba81681c5390432a64a514131ca2dbae2c5a65
Avinash Jha <javinash087@gmail.com>
2020-10-07 21:04:45 +0530

    Updates

---
+++ precache-manifest.a1b2e328631ae1a13393eef95e1fac23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cf461b268030bc9be8f20f273afede",
+    "url": "/pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "fe2f16221ab0b21b7fd0",
+    "url": "/pixabay-image-finder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "53e2820134d0c31f5f58",
+    "url": "/pixabay-image-finder/static/js/2.80d5f62b.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/pixabay-image-finder/static/js/2.80d5f62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2f16221ab0b21b7fd0",
+    "url": "/pixabay-image-finder/static/js/main.408f02d0.chunk.js"
+  },
+  {
+    "revision": "36e2c087759e90c5f0f0",
+    "url": "/pixabay-image-finder/static/js/runtime-main.84dbef6a.js"
+  }
+]);
